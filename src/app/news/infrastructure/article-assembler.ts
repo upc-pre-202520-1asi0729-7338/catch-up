7ab2dd62bd4d0fbf6b84371c62815253eea0a2c8
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-16 11:55:05 -0500

    feat(logo): add response and entity assemblers.

---
+++ src/app/news/infrastructure/article-assembler.ts
@@ -0,0 +1,32 @@
+import {LogoApi} from '../../shared/infrastructure/logo-api';
+import {ArticleResource, TopHeadlinesResponse} from './top-headlines-response';
+import {Article} from '../domain/model/article.entity';
+
+export class ArticleAssembler {
+
+  static logoApi: LogoApi;
+
+  static withLogoApi(logoApi: LogoApi) {
+    this.logoApi = logoApi;
+  }
+
+  static toEntityFromResource(resource: ArticleResource): Article {
+    return {
+      source: {
+        id: resource.source.id || '',
+        name: resource.source.name,
+        url: '',
+        urlToLogo: ''
+      },
+     title: resource.title,
+     description: resource.description || '',
+     url: resource.url,
+     urlToImage: resource.urlToImage || '',
+     publishedAt: resource.publishedAt,
+    };
+  }
+
+  static toEntitiesFromResponse(response: TopHeadlinesResponse): Article[] {
+    return response.articles.map(article => this.toEntityFromResource(article));
+  }
+}
