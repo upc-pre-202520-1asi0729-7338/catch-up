adfc2a264f12b0e347cd92571f67778e73e7bf89
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-16 11:24:03 -0500

    chore: add environment variable files.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: 'YOUR_API_KEY',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/',
+};
