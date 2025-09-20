43a2be5b998b353b822eca8654375f869cddb403
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-20 16:10:47 -0500

    feat(layout): integrate layout component into app.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import {Component, signal} from '@angular/core';
+import {Layout} from './shared/presentation/layout/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [
+    Layout
+  ],
   styleUrl: './app.css'
 })
 export class App {
